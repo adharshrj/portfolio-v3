c509ae5d74a12720c61ada7451d1db9b1f0d763a
Adharsh <81523460+adharshrj@users.noreply.github.com>
2023-01-02 13:51:21 +0530

    ui cleanup

--- portfolio/pages/components/Skills.jsx
+++ portfolio/pages/components/Skills.jsx
@@ -36,25 +36,25 @@ const otherlist = other.map((value) => {
 
 const Skills = () => {
   return (
-    <div id="skills" className="w-full px-4 lg:h-screen">
+    <div id="skills" className="w-full px-4 pt-16 lg:h-screen">
       <div className="max-w-[1240px] mx-auto flex flex-col justify-center h-full">
         <p className="font-extrabold text-xl tracking-widest uppercase text-[#3b95da]">
           Skills
         </p>
         <h2 className="py-4 text-[#dcdbdb]">My Forte</h2>
-        <p className="font-extrabold text-xl tracking-widest uppercase text-[#3b95da] py-4"> Frontend</p>
+        <p className="font-extrabold text-xl tracking-widest uppercase text-[#3b95da] "> Frontend</p>
         <div className="grid grid-cols-2 lg:grid-cols-4 gap-8 text-[#dcdbdb]">
           {frontendlist}
         </div>
-        <p className="font-extrabold text-xl tracking-widest uppercase text-[#3b95da] py-4"> Backend</p>
+        <p className="font-extrabold text-xl tracking-widest uppercase text-[#3b95da] sm:pt-8 md:pt-8"> Backend</p>
         <div className="grid grid-cols-2 lg:grid-cols-4 gap-8 text-[#dcdbdb]">
           {backendlist}
         </div>
-        <p className="font-extrabold text-xl tracking-widest uppercase text-[#3b95da] py-4"> Database</p>
+        <p className="font-extrabold text-xl tracking-widest uppercase text-[#3b95da] sm:pt-8 md:pt-8"> Database</p>
         <div className="grid grid-cols-2 lg:grid-cols-4 gap-8 text-[#dcdbdb]">
           {dblist}
         </div>
-        <p className="font-extrabold text-xl tracking-widest uppercase text-[#3b95da] py-4"> Platform</p>
+        <p className="font-extrabold text-xl tracking-widest uppercase text-[#3b95da] sm:pt-8 md:pt-8"> Platform</p>
         <div className="grid grid-cols-2 lg:grid-cols-4 gap-8 text-[#dcdbdb]">
           {otherlist}
         </div>
